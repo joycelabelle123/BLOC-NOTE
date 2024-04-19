b835bca526ac131f8c21202d6e38e93253a8fd12
joycelabelle123 <joyceperelle0@gmail.com>
2024-04-19 14:19:54 +0200

    commit de joyce

--- bloc.py
+++ bloc.py
@@ -1,1 +1,13 @@
-import tkinter as tk 
+import tkinter as tk
+import tkinter as dialog
+root = tk.Tk()
+root.title("bloc-note")
+root.geometrie("500x500")
+#creer une barre de menu
+menu_bar = tk.Menu(root)
+#ajout d'un menu deroulant a la barre de menu
+fichier_menu = tk.Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="fichier",menu=fichier_menu)
+#ajout des elements du menu
+fichier_menu.add_command(label="gin")
+root.mainloop()
